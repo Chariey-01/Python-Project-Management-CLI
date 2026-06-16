9d612ae643d981090aea970b6120672c15934b42
Chariey-01 <charityjepkoech007@gmail.com>
2026-06-17 02:22:42 +0300

    Project and task CLI commands implemented

--- main.py
+++ main.py
@@ -17,6 +17,15 @@ add_user_parser = subparsers.add_parser(
     help="Add a new user"
 )
 
+list_users_parser = subparsers.add_parser(
+    "list-users",
+    help="Display all users"
+)
+
+list_projects_parser = subparsers.add_parser(
+    "list-projects",
+    help="Display all projects"
+)
 add_user_parser.add_argument("--name", required=True)
 add_user_parser.add_argument("--age", type=int, required=True)
 add_user_parser.add_argument("--email")
@@ -37,5 +46,112 @@ if args.command == "add-user":
         parser.error(str(error))
 
     print("User added successfully")
+
+
+elif args.command == "list-users":
+
+    users = manager.get_all_users()
+
+    if not users:
+        print("No users found")
+
+    for user in users:
+        print(user)
+
+elif args.command == "add-project":
+
+    manager.create_project(
+        args.title,
+        args.description,
+        args.start_date,
+        args.end_date
+    )
+
+    print("Project added successfully")
+
+elif args.command == "list-projects":
+
+    projects = manager.get_all_projects()
+
+    if not projects:
+        print("No projects found")
+
+    for project in projects:
+        print(project)   
+
+elif args.command == "complete-task":
+
+    success = manager.complete_task(
+        args.project_id,
+        args.task_id
+    )
+
+    if success:
+        print("Task completed")
+    else:
+        print("Task not found")
+
 else:
     parser.print_help()
+# Add project comand
+add_project_parser = subparsers.add_parser(
+    "add-project",
+    help="Create a project"
+)
+
+add_project_parser.add_argument(
+    "--title",
+    required=True
+)
+
+add_project_parser.add_argument(
+    "--description",
+    required=True
+)
+
+add_project_parser.add_argument(
+    "--start-date",
+    required=True
+)
+
+add_project_parser.add_argument(
+    "--end-date",
+    required=True
+)
+# Add task command
+add_task_parser = subparsers.add_parser(
+    "add-task",
+    help="Add task to project"
+)
+
+add_task_parser.add_argument(
+    "--project-id",
+    type=int,
+    required=True
+)
+
+add_task_parser.add_argument(
+    "--title",
+    required=True
+)
+
+add_task_parser.add_argument(
+    "--description",
+    required=True
+)
+# Complete task command
+complete_task_parser = subparsers.add_parser(
+    "complete-task"
+)
+
+complete_task_parser.add_argument(
+    "--project-id",
+    type=int,
+    required=True
+)
+
+complete_task_parser.add_argument(
+    "--task-id",
+    type=int,
+    required=True
+)
